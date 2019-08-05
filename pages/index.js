8839ec2b16815ef2bd35c65461636790a40d6ffc
luandevpro <luandevpro@gmail.com>
2019-08-05 21:35:55 +0700

    template nextjs material ui v4 eslint

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import TextField from '@material-ui/core/TextField';
+
+export default () => (
+  <div style={{ textAlign: 'center', margin: '100px' }}>
+    <TextField label="Custom CSS" variant="outlined" id="custom-css-outlined-input" />
+  </div>
+);
