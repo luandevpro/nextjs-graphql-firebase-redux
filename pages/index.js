36b7a0842f51b6603cc5d3f9cfabb6ce38f61ce1
luandevpro <tranluan1c@gmail.com>
2019-10-04 17:36:42 +0700

    add firebase remote config

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,22 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import withApollo from '../lib/withApollo';
 import withLayout from '../lib/withLayout';
+import { remoteConfig } from '../lib/firebase';
 
 const Index = () => {
   const userCurrentUser = useSelector((state) => state.currentUser);
+
+  if (process.browser) {
+    remoteConfig
+      .fetchAndActivate()
+      .then(() => {
+        console.log(remoteConfig.getAll());
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   return (
     <div style={{ textAlign: 'center', color: 'black' }}>
       {userCurrentUser ? (
