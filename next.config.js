3508bbd2328db21027de46ed3c704f9f4150b64c
luandevpro <tranluan1c@gmail.com>
2019-10-10 09:37:15 +0700

    fix env client

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const withOffline = require('next-offline');
 
-module.exports = withPlugins([withCSS, withOffline]);
+const withEnv = {
+  publicRuntimeConfig: {
+    HASURA_GRAPHQL_ENGINE_HOSTNAME: 'nextjs-graphql-firebase-redux.herokuapp.com',
+  },
+};
+module.exports = withPlugins([withCSS, withOffline, withEnv]);
